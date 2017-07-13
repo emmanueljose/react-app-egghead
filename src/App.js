acf1b6f655ae8e33ca85c138d83c6cc568b06876
Jose <joseem@ga.com>
2017-07-13 14:35:42 -0700

    Completed Lesson 15

--- src/App.js
+++ src/App.js
@@ -1,30 +1,63 @@
 import React from 'react';
 
+const HOC = (InnerComponent) => class extends React.Component{
+  constructor(){
+    super();
+    this.state = {count: 0}
+  }
+  update(){
+    this.setState(
+      {
+        count: this.state.count + 1
+      }
+    )
+  }
+  componentWillMount(){
+    //Should print twice. 1 for button, 1 for label
+    console.log('Will Mount')
+  }
+  render(){
+    return(
+      <InnerComponent 
+        {...this.props}
+        {...this.state}
+        update={this.update.bind(this)}
+      />
+    )
+  }
+}
+
 class App extends React.Component{
   render(){
     return (
       <div>
         <Button>button</Button>
         <hr/>
-        <Label>label</Label>
+        <LabelHOC>label</LabelHOC>
       </div>
     )
   }
 }
 
-const Button = (props) => <button>{props.children}</button>
+const Button = HOC((props) => 
+  <button onClick={props.update}>{props.children} - {props.count}</button>
+)
 
 class Label extends React.Component{
+  componentWillMount(){
+    console.log('label will mount')
+  }
   render(){
     return (
-      <label>{this.props.children}</label>
+      <label onMouseMove={this.props.update}>{this.props.children} - {this.props.count}</label>
     )
   }
 }
 
+const LabelHOC = HOC(Label);
 
 // const Button = (props) => <button>{props.children}</button>
-// *NOTE: Both button and labele do the same thing just differently.
+// *NOTE: Both button and labele do the same thing just differently
 // class Label extends React.Component{
 //   render(){
 //     return (
